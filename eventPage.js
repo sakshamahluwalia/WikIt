5a2f7340ba69c9cefb67a53cd9f70081ef6ee307
ahluwa41 <saksham.ahluwalia@mail.utoronto.ca>
2018-11-11 19:26:19 -0500

    intial commit

---
+++ eventPage.js
@@ -0,0 +1,29 @@
+var menuItem = {
+	"id": "wikit",
+	"title": "wikit",
+	"contexts": ["selection"]
+};
+
+chrome.contextMenus.create(menuItem);
+
+
+function fixedEncodeURI (str) {
+	return encodeURI(str).replace(/%5B/g, '[').replace(/%5D/g, ']');
+}
+
+chrome.contextMenus.onClicked.addListener(function(clickData) {
+	if (clickData.menuItemId == "wikit" && clickData.selectionText) {
+		var wikiUrl = "https://en.wikipedia.org/wiki/" + fixedEncodeURI(clickData.selectionText);
+		var createData = {
+			"url": wikiUrl,
+			"type": "normal",
+			"top": 5,
+			"left": 5,
+			"width": screen.availWidth/2,
+			"height": screen.availHeight/2
+		}
+
+		chrome.windows.create(createData, function() {});
+	}
+
+});
